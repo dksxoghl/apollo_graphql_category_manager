e51b6bf807c1b2b3ad8791d64c70f8c77d0e97b0
dksxoghl <dksxoghl8@gmail.com>
2020-02-10 11:58:54 +0900

    조건추가 및 같은 depth끼리 이동, order필드 바꾸기 기능동작

--- src/Category/SettingContainer/MoveDown.ts
+++ src/Category/SettingContainer/MoveDown.ts
@@ -1,15 +1,20 @@
-export const MoveDown = (contents, value, type) => {
-    console.log(contents,'순서~~~~~~~~~~~',value,type)
+export const MoveDown = (contents, value, type,child,behind) => {
+    console.log(contents,'순서~~~~~~~~~~~',value,type,child.length,behind.length)
     const index = contents.indexOf(value);
-    let newPos = type? index - 1 : index + 1
-
-    if (index === -1) 
-         throw new Error("Element not found in  content");
+    let newPos = type? index - behind.length : index + behind.length
+    // contents.filter(list => list.parent_id === item.id);
+    // if (index === -1) 
+    //      throw new Error("Element not found in  content");
     
     const newContents = JSON.parse(JSON.stringify(contents));
     if (newPos >= contents.length)
         newPos = contents.length;
-    newContents.splice(index, 1);
-    newContents.splice(newPos, 0, value);
-    return newContents;
+    newContents.splice(index, child.length);
+    for (let index = 0; index < child.length; index++) {
+        newContents.splice(newPos+index, 0, child[index]);
+    }
+    const newOrder=newContents.map((item,index) => {                 //집어넣는 위치 이후에있는 순서값들 1씩증가
+        return { ...item, order: (index + 1) }; 
+    });
+    return newOrder;
 };
--- src/Category/SettingContainer/SettingBox.tsx
+++ src/Category/SettingContainer/SettingBox.tsx
@@ -72,15 +72,49 @@ function SettingBox({ categories, onSave }) {
         })
     }
     const orderChange = (item, type) => {
-        if((item.order===1 && type)||item.order===subMenu.category.length && !type) return;
-        if(type&&subMenu.category[item.order-2].id===item.parent_id){
+        if ((item.order === 1 && type) || item.order === subMenu.category.length && !type) return alert('최상위, 최하위입니다.');
+        if (type && subMenu.category[item.order - 2].id === item.parent_id) {       //위로갈때 
             return alert('같은 그룹끼리만 이동가능');
         }
-        if(!type&&subMenu.category[item.order].id.length<item.id.length){
+        if (!type && subMenu.category[item.order].id.length < item.id.length) { //아래로갈때
             return alert('같은 그룹끼리만 이동가능');
         }
-        let parentList = subMenu.category.filter(list => list.parent_id === item.id);
-        setSubMenu({ category: MoveDown(subMenu.category, item, type) })
+        let child = subMenu.category.filter(list => {
+            let len = item.id.length;
+            if (list.id.slice(0, len) === item.id) return list;
+        });
+        let below;
+        if (!type) {
+            below = subMenu.category.slice((item.order - 1) + child.length, (item.order - 1) + child.length + 1);
+            if (!type && below[0].id.length < item.id.length) {             // 아래로갈때 다른 대 중분류일시 수정불가
+                return alert('같은 그룹끼리만 이동가능');
+            }
+        }else{
+            let parentList = subMenu.category.filter(list => list.parent_id === item.parent_id);
+            console.log(parentList,item);
+            let findIndex=parentList.indexOf(item);     
+            console.log(findIndex);
+            below=[parentList[findIndex-1]];
+            // let arr=item.id.split(':');                         
+            // let findId= parseInt(arr[arr.length-1].slice(1,arr[arr.length-1].length))-1;  
+            // let find= 'a'+findId;
+            // let id='';
+            // for (let index = 0; index < arr.length-1; index++) {
+            //     id += (arr[index]+':');
+            // } 
+            // id+=find;
+            // console.log(id);
+            // // let findId= parseInt(item.id);
+            // below = subMenu.category.filter(list => list.id === id);
+            console.log(below);
+        }
+        let behind = subMenu.category.filter(list => {
+            let len = below[0].id.length;
+            if (list.id.slice(0, len) === below[0].id) return list;
+        });
+        console.log(child);
+        // let parentList = subMenu.category.filter(list => list.parent_id === item.id);
+        setSubMenu({ category: MoveDown(subMenu.category, item, type, child, behind) })
     }
     const onAdd = (id) => {
         console.log(id);
@@ -136,69 +170,65 @@ function SettingBox({ categories, onSave }) {
         }
     }
     console.log(deleteId);
-    
+
     const addSub = (id) => {
         let parentList = subMenu.category.filter(list => list.parent_id === id);  //하위항목들구하고
         let newId = '';
         if (parentList.length > 0) {
-             let arr = parentList[parentList.length - 1].id.split(':');                  //:하위항목 마지막꺼 구분자로 자르고
-             let selectArr = arr[arr.length - 1].slice(1, arr[arr.length - 1].length);        //아이디의 마지막꺼 문자도자르고
-             let endId = 'a' + (parseInt(selectArr) + 1);
-             for (let index = 0; index < arr.length - 1; index++) {
-                 newId += arr[index] + ':';
-             }
-             newId += endId;
+            let arr = parentList[parentList.length - 1].id.split(':');                  //:하위항목 마지막꺼 구분자로 자르고
+            let selectArr = arr[arr.length - 1].slice(1, arr[arr.length - 1].length);        //아이디의 마지막꺼 문자도자르고
+            let endId = 'a' + (parseInt(selectArr) + 1);
+            for (let index = 0; index < arr.length - 1; index++) {
+                newId += arr[index] + ':';
+            }
+            newId += endId;
         }
-        else{
-            newId=id+':a0';
+        else {
+            newId = id + ':a0';
             parentList = subMenu.category.filter(list => list.id === id);
         }
         console.log(parentList);
-           
-            let parent = {
-                id: newId,
-                name: "수정요망",
-                parent_id: id,
-                status: "show",
-                order: parentList[parentList.length - 1].order + 1,
-                __typename: "categories2"
 
-            }
-            let num = parentList[parentList.length - 1].order;
-            let addArr = subMenu.category.map(item => {                 //집어넣는 위치 이후에있는 순서값들 1씩증가
-                if (item.order < parent.order) { return item; }
-                else { return { ...item, order: (item.order + 1) }; }
-            });
+        let parent = {
+            id: newId,
+            name: "수정요망",
+            parent_id: id,
+            status: "show",
+            order: parentList[parentList.length - 1].order + 1,
+            __typename: "categories2"
 
-            // setSubMenu({
-            //     category: subMenu.category.map(item=>{
-            //         if(item.order<parent.order) { return item;}
-            //         else {return {...item, order: (item.order+1)};}
-            //     })
-            // });
-            setSubMenu({ category: [...addArr.slice(0, num), parent, ...addArr.slice(num, subMenu.category.length)] }); //객체를 배열 원하는위치안에 삽입
-        
-       
+        }
+        let num = parentList[parentList.length - 1].order;
+        let addArr = subMenu.category.map(item => {                 //집어넣는 위치 이후에있는 순서값들 1씩증가
+            if (item.order < parent.order) { return item; }
+            else { return { ...item, order: (item.order + 1) }; }
+        });
+
+        // setSubMenu({
+        //     category: subMenu.category.map(item=>{
+        //         if(item.order<parent.order) { return item;}
+        //         else {return {...item, order: (item.order+1)};}
+        //     })
+        // });
+        setSubMenu({ category: [...addArr.slice(0, num), parent, ...addArr.slice(num, subMenu.category.length)] }); //객체를 배열 원하는위치안에 삽입
+
+
     }
-//전체객체관리 
+    //전체객체관리 
     useEffect(() => {
         let parentList = categories.filter(list => list.parent_id === selectID);
-        // if(subMenu===undefined) return;
-        // setSubMenu(parentList)
-        // setSubMenu(subMenu.category.push(parentList))
-        if (parentList.length > 1) {
-            let b = true;     //이미 추가된 목록이라면 추가불가
-            subMenu.category.map((item) => {
-                if (item.parent_id === selectID) b = false;
-            })
-            // parentList.map((item)=>{
-            // subMenu.category.concat(item);
-            if (b) (
-                setSubMenu({ category: subMenu.category.concat(parentList) })
-            )
-            // })
-        }
-        //  setSubMenu(old=>[...old,parentList]); 
+
+        // if (parentList.length > 1) {
+        //     let b = true;     //이미 추가된 목록이라면 추가불가
+        //     subMenu.category.map((item) => {
+        //         if (item.parent_id === selectID) b = false;
+        //     })
+
+        //     if (b) (
+        //         setSubMenu({ category: subMenu.category.concat(parentList) })
+        //     )
+        // }
+        // //  setSubMenu(old=>[...old,parentList]); 
         console.log(subMenu, parentList, '~~~~~');
     }, [selectID, addSub])
 
