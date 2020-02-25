d681894f5849d3e7065095400e7d016310395aa7
dksxoghl <dksxoghl8@gmail.com>
2020-02-25 17:28:21 +0900

    link change

--- src/graphql/apollo.ts
+++ src/graphql/apollo.ts
@@ -3,7 +3,6 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import { createHttpLink,HttpLink  } from "apollo-link-http";
 
 const link = createHttpLink({ uri: "https://apollo-category.herokuapp.com/v1/graphql",
-// headers: {"x-hasura-admin-secret":"1234"}
 });
 // link: "http://121.134.238.182:26060/console",
 export const client = new ApolloClient({
