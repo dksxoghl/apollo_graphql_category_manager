c12d0d4828603264b8a29588997987d0ee9c18b0
dksxoghl <dksxoghl8@gmail.com>
2020-02-25 17:25:54 +0900

    link change

--- src/graphql/graphql.ts
+++ src/graphql/graphql.ts
@@ -1,106 +1,83 @@
 import gql from "graphql-tag";
 
-export const DELETE_CATEGORY = gql`
-mutation MyMutation2($id: String!) {
-  delete_categories(id: $id)
-  @rest(
-    type:"categories2",  path:"category/{args.id}",
-    method:"DELETE"
-  ) {
-    id
-  }
-}
-`;
-// export const DELETE_CATEGORY = gql`
-// mutation MyMutation2($id: String!) {
-//   delete_categories(where: {_or: [{id: {_eq: $id}}, {parent_id: {_eq: $id}}]})
-//   @rest(
-//     type: "categories2", path: "category/{id}",
-//     method: "DELETE"
-//   ) {
-//     id
+// export const GET_CATEGORY = gql`
+// query MyQuery {
+//     __typename
+//     categories {
+//       id
+//       name
+//       parent_id
+//     }
 //   }
-// }
 // `;
-
-export const GET_CATEGORY = gql`
-query MyQuery {
-  categories @rest(type: "categories2", path: "category/list") {
-    id
-    name
-    order
-    parent_id
-    status
-    active
-  }
-}
-`
 
 // export const GET_CATEGORY = gql`
 // query MyQuery {
-//   __typename
-//   categories(order_by: {order: asc}) {
+//   categories(where: {}, order_by: {id: asc}) {
 //     id
 //     name
-//     order
 //     parent_id
-//     status
-//     active
+//     enum_categories_status {
+//       value
+//     }
 //   }
 // }
 // `
-export const UPDATE_CATEGORY = gql`
-mutation MyMutation($id: String!,$name:String!,$parent_id:String, $order:Int!,$status:String!,$active:Boolean) {
-  update_categories(body:{id: $id,name: $name, parent_id: $parent_id, order: $order,status:$status,active:$active})
-  @rest(
-    type: "categories2", path: "category/{id}", bodyKey: "body"
-    method: "PUT"
-  ){
+
+// export const GET_SUBCATEGORY = gql`
+// query MyQuery {
+//   categories(where: {parent_id: {_eq: "a0"}}) {
+//     id
+//     name
+//     parent_id
+//   }
+// }
+// `;
+export const DELETE_CATEGORY =gql`
+mutation MyMutation2($id: String!) {
+  delete_categories2(where: {_or: [{id: {_eq: $id}}, {parent_id: {_eq: $id}}]}) {
+    affected_rows
+  }
+}
+`;
+
+
+export const GET_CATEGORY = gql`
+query MyQuery {
+  categories2(where: {}, order_by: {order: asc}) {
     id
     name
-    order
     parent_id
     status
+    order
     active
   }
 }
 `
-export const INSERT_CATEGORY = gql`
+
+export const UPDATE_CATEGORY = gql`
+mutation MyMutation($id: String!,$name:String!,$parent_id:String, $order:Int!,$status:String!,$active:Boolean) {
+  update_categories2(_set: {name: $name, parent_id: $parent_id, order: $order,status:$status,active:$active}, where: {id: {_eq: $id}}) {
+    affected_rows
+  }
+}
+`
+export const INSERT_CATEGORY =gql`
 mutation MyMutation3($id: String!,$name:String!,$parent_id:String, $order:Int!,$status:String!,$active:Boolean) {
-  insert_categories(body:{id: $id,name: $name, parent_id: $parent_id, order: $order,status:$status,active:$active})
-  @rest(type:"categories2", path: "category", method: "POST",bodyKey: "body"
-  )  {
-    id
-    name
-    order
-    parent_id
-    status
-    active
+  insert_categories2(objects: {id: $id,name: $name, parent_id: $parent_id, order: $order,status:$status,active:$active}) {
+    affected_rows
   }
 }
 `;
-// bodyKey:"id",bodyKey:"name",bodyKey:"order",bodyKey:"parent_id",bodyKey:"status",bodyKey:"active"
-// type PublishablePostInput={
-//   id: String, name:String, parent_id:String, order:Number, status:String, active:Boolean
-// }
-// fragment PublishablePostInput on REST {
-//   id: String name:String parent_id:String order:Int status:String active:Boolean
-// }
-// export const INSERT_CATEGORY = gql`
 
-// mutation MyMutation3( $someApiWithACustomBodyKey: PublishablePostInput!  $customBuilder: any) {
-//   insert_categories:publish(someApiWithACustomBodyKey: $someApiWithACustomBodyKey)
-//   @rest(type: "categories2" path: "category" method: "POST"
-//   bodyBuilder:$customBuilder)  {
+// query MyQuery2 {
+//   __typename
+//   categories(where: {_or: [{id: {_eq: "a0"}}, {id: {_eq: "a1"}}] }) {
 //     id
 //     name
-//     order
 //     parent_id
 //     status
-//     active
 //   }
 // }
-// `;
-
 
 
